61084d0a6c9fe2b4f14c5ce9b308c8ec6e280529
Michal Piechowiak <misiek.piechowiak@gmail.com>
2024-02-01 12:42:19 +0100

    wat

--- pages/ssr.js
+++ pages/ssr.js
@@ -1,15 +1,18 @@
 export async function getServerSideProps() {
+  const { default: esmOnlyStuff } = await import("parse-json");
+
   return {
     props: {
       ssr: true,
+      esmTest: esmOnlyStuff(`{"test": "esm"}`),
     },
   };
 }
 
-const SSR = ({ ssr }) => {
+const SSR = ({ ssr, esmTest }) => {
   return (
     <div>
-      <pre>{JSON.stringify({ ssr }, null, 2)}</pre>
+      <pre>{JSON.stringify({ ssr, esmTest }, null, 2)}</pre>
     </div>
   );
 };
